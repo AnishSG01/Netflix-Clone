a431004699b4386ee3cb11ea3e15045d9ac89fd9
Anish <ghogareanish@gmail.com>
2022-08-17 01:11:23 +0530

    Basics Done - UI is incomplete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,53 +1,78 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Home.scss";
+import axios from "axios";
+
+const key = '5316f94b0a82460f632ac35ffed406fa';
+const moviesUrl = 'https://api.themoviedb.org/3/movie';
+const popular = 'popular';
+const upcoming = 'upcoming';
+const latest = 'now_playing';
+const top_rated = 'top_rated';
+const Card = ({ image }) => <img src={image} className="card" alt="cover" />;
+const imgUrl = "https://image.tmdb.org/t/p/original";
+const Row = ({
+  title,
+  arr = [],
+}) => (
+  <div className="row">
+    <h2>{title}</h2>
+
+    <div className="cols">
+      {arr.map((item, index) => (
+        <Card key={index} image={`${imgUrl}/${item.poster_path}`} />
+      ))}
+    </div>
+  </div>
+);
 
 const Home = () => {
-  const Card = ({ image }) => <img src={image} className="card" alt="cover" />;
 
-  const Row = ({
-    title,
-    arr = [
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-      {
-        img: "https://assets.nflxext.com/us/boxshots/hd1080/81058097.jpg",
-      },
-    ],
-  }) => (
-    <div className="row">
-      <h2>{title}</h2>
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [topRatedMovies, settopRatedMovies] = useState([]);
+  const [latestMovies, setLatestMovies] = useState([]);
+  useEffect(() => {
 
-      <div className="cols">
-        {arr.map((item) => (
-          <Card image={item.img} />
-        ))}
-      </div>
-    </div>
-  );
+    const fetchPopular = async ()=>{
+      const { data: {results}} = await axios.get(`${moviesUrl}/${popular}?api_key=${key}&language=en-US&page=1`);
+      setPopularMovies(results);
+    }
+
+    const fetchtopRated = async ()=>{
+      const { data: {results}} = await axios.get(`${moviesUrl}/${top_rated}?api_key=${key}&language=en-US&page=1`);
+      settopRatedMovies(results);
+    }
+    
+    const fetchupcoming = async ()=>{
+      const { data: {results}} = await axios.get(`${moviesUrl}/${upcoming}?api_key=${key}&language=en-US&page=1`);
+      setUpcomingMovies(results);
+    }
+    
+    const fetchLatest = async ()=>{
+      const { data: {results}} = await axios.get(`${moviesUrl}/${latest}?api_key=${key}&language=en-US&page=1`);
+      setLatestMovies(results);
+    }
+
+    fetchPopular();
+    fetchupcoming();
+    fetchtopRated();
+    fetchLatest();
+});
+  
 
   return (
     <div className="home">
-      <div className="banner"></div>
+      <div className="banner" style={{
+        backgroundImage: `url(${imgUrl}/ujr5pztc1oitbe7ViMUOilFaJ7s.jpg)`
+      }}>
+        
+      </div>
 
-      <Row title={"Popular on Netflix"} />
-      <Row title={"Movies"} />
-      <Row title={"Recently Added"} />
+      <Row title={"Popular on Netflix"} arr={popularMovies} />
+      <Row title={"Top - Rated"} arr={topRatedMovies} />
+      <Row title={"Latest"} arr={latestMovies} />
+      <Row title={"Movies"} arr={upcomingMovies} />
+      
     </div>
   );
 };
